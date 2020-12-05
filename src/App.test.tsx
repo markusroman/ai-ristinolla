cee0f923d3a26826bf01f759be355db2b2e89bc7
Markus Röman <markus.roman@hotmail.fi>
2020-12-05 18:32:38 +0200

    Changed tests to use snapshots

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,20 +1,10 @@
+import { render } from '@testing-library/react';
 import React from 'react';
-import { screen, render, fireEvent } from '@testing-library/react';
+import { snapshot_UNSTABLE, RecoilRoot } from 'recoil';
 import App from './App';
-import { RecoilRoot } from 'recoil';
+import { board } from './store';
 
 const EMPTY_BOARD = [null, null, null, null, null, null, null, null, null];
-
-let board_state: (null | string)[] = [];
-
-const getBoardState = () => {
-  let tiles = screen.queryAllByTestId(/tile-/i);
-  let state = tiles.map((item) => {
-    let value = item.getAttribute('id')?.split('-')[3];
-    return value === 'null' ? null : String(value);
-  });
-  return state;
-};
 
 beforeEach(() => {
   render(
@@ -24,7 +14,6 @@ beforeEach(() => {
       </RecoilRoot>
     </React.StrictMode>
   );
-  board_state = getBoardState();
 });
 
 it('renders the app correctly', () => {
@@ -37,14 +26,8 @@ it('renders the app correctly', () => {
 });
 
 it('rendered board is empty', () => {
-  expect(board_state).toEqual(EMPTY_BOARD);
-});
-
-it('clicked tile changes value', () => {
-  expect(board_state).toEqual(EMPTY_BOARD);
-  let tiles = screen.queryAllByTestId(/tile-/i);
-  fireEvent(tiles[4], new MouseEvent('click'));
-  board_state = getBoardState();
-  console.log(board_state, tiles[4]);
-  expect(board_state[4]).toEqual('X');
+  const initialSnapshot = snapshot_UNSTABLE();
+  expect(initialSnapshot.getLoadable(board).valueOrThrow()).toEqual(
+    EMPTY_BOARD
+  );
 });
